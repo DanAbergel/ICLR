fb0c8208aec77268b88ac8a0840da58abf4d7cf8
danabergel <danabergel1995@gmail.com>
2025-12-16 18:27:06 +0200

    change architecture of MLP

--- models/heads.py
+++ models/heads.py
@@ -1,26 +1,47 @@
+# import torch.nn as nn
+#
+#
+# class PredictionHead(nn.Module):
+#     def __init__(self, embedding_dim, output_dims, p_dropout=0.2):
+#         super().__init__()
+#
+#         self.input_dim = embedding_dim
+#         self.hidden_dim1 = embedding_dim // 2
+#         # self.hidden_dim2 = embedding_dim // 4
+#
+#         self.task_heads = nn.ModuleDict()
+#         for task, output_dim in output_dims.items():
+#             self.task_heads[task] = nn.Sequential(
+#                 nn.Linear(self.input_dim, self.hidden_dim1),
+#                 nn.GELU(),
+#                 nn.Dropout(p_dropout),
+#                 nn.Linear(self.hidden_dim1, self.hidden_dim2),
+#                 nn.GELU(),
+#                 # nn.Dropout(p_dropout),
+#                 # nn.Linear(self.hidden_dim2, output_dim),
+#             )
+#
+#     def forward(self, x):
+#         task_predictions = {task: head(x) for task, head in self.task_heads.items()}
+#         return task_predictions
+
+
 import torch.nn as nn
 
-
 class PredictionHead(nn.Module):
-    def __init__(self, embedding_dim, output_dims, p_dropout=0.2):
+    def __init__(self, embedding_dim, output_dims, p_dropout=0.5):
         super().__init__()
 
-        self.input_dim = embedding_dim
-        self.hidden_dim1 = embedding_dim // 2
-        self.hidden_dim2 = embedding_dim // 4
+        hidden_dim = 64  # volontairement petit
 
         self.task_heads = nn.ModuleDict()
         for task, output_dim in output_dims.items():
             self.task_heads[task] = nn.Sequential(
-                nn.Linear(self.input_dim, self.hidden_dim1),
-                nn.GELU(),
+                nn.Linear(embedding_dim, hidden_dim),
+                nn.ReLU(),
                 nn.Dropout(p_dropout),
-                nn.Linear(self.hidden_dim1, self.hidden_dim2),
-                nn.GELU(),
-                nn.Dropout(p_dropout),
-                nn.Linear(self.hidden_dim2, output_dim),
+                nn.Linear(hidden_dim, output_dim),
             )
 
     def forward(self, x):
-        task_predictions = {task: head(x) for task, head in self.task_heads.items()}
-        return task_predictions
+        return {task: head(x) for task, head in self.task_heads.items()}
